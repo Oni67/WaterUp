9d14546da737c9aef7115ef5074390a8bc46e90f
4xFire99 <2095219@student.uma.pt>
2024-05-19 22:42:04 +0100

    build: Works perfectly

--- waterup/lib/pages/Water_History.dart
+++ waterup/lib/pages/Water_History.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:waterup/backend/AddWaterPage.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:intl/intl.dart';
 
 class WaterHistoryPage extends StatelessWidget {
-  const WaterHistoryPage({Key? key});
+  const WaterHistoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,48 +17,74 @@ class WaterHistoryPage extends StatelessWidget {
 }
 
 class WaterHistory extends StatefulWidget {
-  const WaterHistory({Key? key});
+  const WaterHistory({Key? key}) : super(key: key);
 
   @override
   WaterHistoryState createState() => WaterHistoryState();
 }
 
-class BudgetList {
-  List<String> waterHistory;
-
-  BudgetList() : waterHistory = [];
-
-  BudgetList.withInitialValues(List<String> initialValues)
-      : waterHistory = initialValues;
-
-  String? get first => null;
-}
-
 class WaterHistoryState extends State<WaterHistory> {
+  late Future<Map<String, double>> _weeklyDataFuture;
+  late Future<List<DocumentSnapshot>> _listDataFuture;
   late List<bool> _selected;
   late DateTime _currentWeek;
-  final int listLength = BudgetList().waterHistory.length;
 
   @override
   void initState() {
     super.initState();
     _currentWeek = DateTime.now();
-    initializeSelection();
-  }
-
-  void initializeSelection() {
-    _selected = List<bool>.generate(listLength, (_) => false);
+    _weeklyDataFuture = _fetchWeeklyData();
+    _listDataFuture = _fetchListData();
   }
 
   void _previousWeek() {
     setState(() {
       _currentWeek = _currentWeek.subtract(const Duration(days: 7));
+      _weeklyDataFuture = _fetchWeeklyData();
+      _listDataFuture = _fetchListData();
     });
   }
 
   void _nextWeek() {
     setState(() {
       _currentWeek = _currentWeek.add(const Duration(days: 7));
+      _weeklyDataFuture = _fetchWeeklyData();
+      _listDataFuture = _fetchListData();
+    });
+  }
+
+  void _editEntry(DocumentSnapshot entry) async {
+    final defaultData = {
+      'Data do registo': entry['Data do registo'],
+      'Quantidade de água (mL)': entry['Quantidade de água (mL)'],
+    };
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddWaterScreen(
+          transactionData: defaultData,
+          control: true,
+          controlDocId: entry.id,
+          budgets: [],
+        ),
+      ),
+    );
+    setState(() {
+      _weeklyDataFuture = _fetchWeeklyData();
+      _listDataFuture = _fetchListData();
+    });
+  }
+
+  void _removeEntry(DocumentSnapshot entry) async {
+    await FirebaseFirestore.instance
+        .collection('WaterHistory')
+        .doc(FirebaseAuth.instance.currentUser?.email)
+        .collection('2024')
+        .doc(entry.id)
+        .delete();
+    setState(() {
+      _weeklyDataFuture = _fetchWeeklyData();
+      _listDataFuture = _fetchListData();
     });
   }
 
@@ -89,14 +116,23 @@ class WaterHistoryState extends State<WaterHistory> {
       ),
       body: Column(
         children: [
+          _buildWeekNavigation(),
+          _buildBarChart(),
           Expanded(
-            child: ListBuilder(
-              selectedList: _selected,
-              onSelectionChange: (bool x) {
-                setState(() {});
+            child: FutureBuilder<List<DocumentSnapshot>>(
+              future: _listDataFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Text('No data available for this week.');
+                } else {
+                  _selected = List<bool>.filled(snapshot.data!.length, false);
+                  return _buildList(snapshot.data!);
+                }
               },
-              buildWeekNavigation: _buildWeekNavigation(),
-              buildBarChart: _buildBarChart(),
             ),
           ),
         ],
@@ -125,72 +161,74 @@ class WaterHistoryState extends State<WaterHistory> {
   }
 
   Widget _buildBarChart() {
-    return FutureBuilder(
-      future: _fetchWeeklyData(),
-      builder: (context, AsyncSnapshot<Map<String, double>> snapshot) {
-        if (!snapshot.hasData) {
+    return FutureBuilder<Map<String, double>>(
+      future: _weeklyDataFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
           return const CircularProgressIndicator();
-        }
-
-        final data = snapshot.data!;
-        final barGroups = data.entries.map((entry) {
-          int dayIndex = _getDayIndex(entry.key);
-          return BarChartGroupData(
-            x: dayIndex,
-            barRods: [
-              BarChartRodData(
-                toY: entry.value,
-                color: Colors.blue,
-                width: 22,
-                backDrawRodData: BackgroundBarChartRodData(
-                  show: true,
-                  toY: 5000, // Adjust this value to match the maximum expected value
-                  color: Colors.grey[300]!,
-                ),
-              ),
-            ],
-          );
-        }).toList();
-
-        return SizedBox(
-          height: 200,
-          child: BarChart(
-            BarChartData(
-              alignment: BarChartAlignment.spaceAround,
-              barGroups: barGroups,
-              titlesData: FlTitlesData(
-                bottomTitles: AxisTitles(
-                  sideTitles: SideTitles(
-                    showTitles: true,
-                    getTitlesWidget: (value, meta) {
-                      final days = [
-                        'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
-                      ];
-                      return SideTitleWidget(
-                        axisSide: meta.axisSide,
-                        child: Text(days[value.toInt()]),
-                      );
-                    },
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Text('No data available for this week.');
+        } else {
+          final data = snapshot.data!;
+          final barGroups = data.entries.map((entry) {
+            int dayIndex = _getDayIndex(entry.key);
+            return BarChartGroupData(
+              x: dayIndex,
+              barRods: [
+                BarChartRodData(
+                  toY: entry.value,
+                  color: Colors.blue,
+                  width: 22,
+                  backDrawRodData: BackgroundBarChartRodData(
+                    show: true,
+                    toY: 5000,
+                    color: Colors.grey[300]!,
                   ),
                 ),
-                leftTitles: AxisTitles(
-                  sideTitles: SideTitles(showTitles: true),
+              ],
+            );
+          }).toList();
+
+          return SizedBox(
+            height: 200,
+            child: BarChart(
+              BarChartData(
+                alignment: BarChartAlignment.spaceAround,
+                barGroups: barGroups,
+                titlesData: FlTitlesData(
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      getTitlesWidget: (value, meta) {
+                        final days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+                        return SideTitleWidget(
+                          axisSide: meta.axisSide,
+                          child: Text(days[value.toInt()]),
+                        );
+                      },
+                    ),
+                  ),
+                  leftTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: true),
+                  ),
+                  topTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                  rightTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
                 ),
-                topTitles: AxisTitles(
-                  sideTitles: SideTitles(showTitles: false),
+                borderData: FlBorderData(
+                  show: true,
+                  border: Border.all(color: Colors.grey, width: 1),
                 ),
-                rightTitles: AxisTitles(
-                  sideTitles: SideTitles(showTitles: false),
-                ),
+                gridData: FlGridData(show: false),
               ),
-              borderData: FlBorderData(
-                show: true,
-                border: Border.all(color: Colors.grey, width: 1),
-              ),
-              gridData: FlGridData(show: false),
             ),
-          ),
-        );
+          );
+        }
       },
     );
   }
@@ -201,11 +239,10 @@ class WaterHistoryState extends State<WaterHistory> {
 
     final querySnapshot = await FirebaseFirestore.instance
         .collection('WaterHistory')
-        .where('Data do registo', isGreaterThanOrEqualTo: DateFormat('yyyy/MM/dd').format(startOfWeek))
-        .where('Data do registo', isLessThanOrEqualTo: DateFormat('yyyy/MM/dd').format(endOfWeek))
+        .doc(FirebaseAuth.instance.currentUser?.email)
+        .collection('2024')
         .get();
 
-    // Initialize all days with 0.0
     final data = {
       'Mon': 0.0,
       'Tue': 0.0,
@@ -220,7 +257,8 @@ class WaterHistoryState extends State<WaterHistory> {
       final dateString = doc['Data do registo'];
       final date = DateFormat('yyyy/MM/dd').parse(dateString);
       final day = DateFormat('EEE').format(date);
-      final quantity = (doc['Quantidade de água (mL)'] as num).toDouble();
+      final quantityString = doc['Quantidade de água (mL)'];
+      final quantity = double.tryParse(quantityString) ?? 0.0;
 
       if (data.containsKey(day)) {
         data[day] = data[day]! + quantity;
@@ -228,6 +266,61 @@ class WaterHistoryState extends State<WaterHistory> {
     }
 
     return data;
+  }
+
+  Future<List<DocumentSnapshot>> _fetchListData() async {
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('WaterHistory')
+        .doc(FirebaseAuth.instance.currentUser?.email)
+        .collection('2024')
+        .get();
+    return querySnapshot.docs;
+  }
+
+  Widget _buildList(List<DocumentSnapshot> entries) {
+    return ListView.builder(
+      itemCount: entries.length,
+      itemBuilder: (context, index) {
+        DocumentSnapshot entry = entries[index];
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: ListTile(
+            leading: const Icon(
+              Icons.opacity,
+              color: Color.fromARGB(255, 0, 174, 255),
+            ),
+            title: Text(entry['Data do registo']),
+            subtitle: Text('${entry['Quantidade de água (mL)']} mL'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.edit, color: Colors.blue),
+                  onPressed: () => _editEntry(entry),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete, color: Colors.red),
+                  onPressed: () => _removeEntry(entry),
+                ),
+                Icon(
+                  _selected[index] ? Icons.check_circle : Icons.radio_button_unchecked,
+                  color: _selected[index] ? Colors.green : Colors.grey,
+                ),
+              ],
+            ),
+            onTap: () {
+              setState(() {
+                _selected[index] = !_selected[index];
+              });
+            },
+          ),
+        );
+      },
+    );
   }
 
   int _getDayIndex(String day) {
@@ -249,148 +342,5 @@ class WaterHistoryState extends State<WaterHistory> {
       default:
         return 0;
     }
-  }
-}
-
-class ListBuilder extends StatefulWidget {
-  const ListBuilder({
-    Key? key,
-    required this.selectedList,
-    required this.onSelectionChange,
-    required this.buildWeekNavigation,
-    required this.buildBarChart,
-  }) : super(key: key);
-
-  final List<bool> selectedList;
-  final ValueChanged<bool>? onSelectionChange;
-  final Widget buildWeekNavigation;
-  final Widget buildBarChart;
-
-  @override
-  State<ListBuilder> createState() => _ListBuilderState();
-}
-
-class _ListBuilderState extends State<ListBuilder> {
-  List<DocumentSnapshot> entries = [];
-  List<String> names = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadData();
-  }
-
-  Future<void> _loadData() async {
-    List<DocumentSnapshot> fetchedData = await getWaterHistoryList();
-    setState(() {
-      entries = fetchedData;
-    });
-  }
-
-  void _toggle(int index) {
-    setState(() {
-      widget.selectedList[index] = !widget.selectedList[index];
-      widget.onSelectionChange!(true);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    names.clear();
-    for (DocumentSnapshot document in entries) {
-      String temp = document['Data do registo'] +
-          ": " +
-          document['Quantidade de água (mL)'].toString() +
-          "mL";
-      names.add(temp);
-    }
-
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(236, 201, 198, 198),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            color: Colors.blue,
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: const Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'WaterUp',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          widget.buildWeekNavigation,
-          widget.buildBarChart,
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: ListView.builder(
-                itemCount: entries.length,
-                itemBuilder: (_, int index) {
-                  return Card(
-                    elevation: 4,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: ListTile(
-                      contentPadding: const EdgeInsets.symmetric(
-                        vertical: 10.0,
-                        horizontal: 15.0,
-                      ),
-                      onTap: () => _toggle(index),
-                      title: Text(
-                        names[index],
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                        ),
-                      ),
-                      trailing: Wrap(
-                        spacing: 10,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.delete, color: Colors.red),
-                            onPressed: () {
-                              deleteTransaction(entries[index].id);
-                              _loadData();
-                            },
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.edit, color: Colors.green),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => AddWaterScreen(
-                                    transactionData: entries[index].data()
-                                        as Map<String, dynamic>,
-                                    control: true,
-                                    controlDocId: entries[index].id,
-                                    budgets: [],
-                                  ),
-                                ),
-                              ).then((_) => _loadData());
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
