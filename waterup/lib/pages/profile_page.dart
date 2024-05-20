e028bbf248e59f423b08b1007554f83a114c0b8d
Andrés Millán <45878709+Oni67@users.noreply.github.com>
2024-05-20 17:21:59 +0100

    Profile page updated
    Weird bug scrolling 3 by 3 in terms of weight and height.

--- waterup/lib/pages/profile_page.dart
+++ waterup/lib/pages/profile_page.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:flutter/cupertino.dart';
 
 class ProfileForm extends StatefulWidget {
   const ProfileForm({Key? key});
@@ -19,6 +20,17 @@ class _ProfileFormState extends State<ProfileForm> {
   final TextEditingController _dobController = TextEditingController();
   DateTime? _selectedDate;
   File? _selectedImage;
+  String exerciseFrequency = 'No exercise';
+  double weight = 70.0; // default weight
+  double height = 170.0; // default height
+
+  final List<String> exerciseOptions = [
+    'No exercise',
+    '1-2 days a week',
+    '3-4 days a week',
+    '5-6 days a week',
+    'Every day',
+  ];
 
   @override
   void initState() {
@@ -37,10 +49,13 @@ class _ProfileFormState extends State<ProfileForm> {
           _nameController.text = snapshot.data()!['Name'] ?? '';
           _emailController.text = snapshot.data()!['Email'] ?? '';
           _dobController.text = snapshot.data()!['Date Of Birth'] ?? '';
+          exerciseFrequency =
+              snapshot.data()!['Exercise Frequency'] ?? 'No exercise';
+          weight = (snapshot.data()!['Weight'] ?? 70).toDouble();
+          height = (snapshot.data()!['Height'] ?? 170).toDouble();
         });
       }
     } catch (e) {
-      // Handle errors
       print('Error loading profile data: $e');
     }
   }
@@ -69,44 +84,40 @@ class _ProfileFormState extends State<ProfileForm> {
         _selectedImage = File(pickedFile.path);
       });
 
-      // Upload the image to Firebase Storage
       await _uploadImageToFirebaseStorage();
     }
   }
 
-Future<void> _uploadImageToFirebaseStorage() async {
-  try {
-    if (_selectedImage != null) {
-      final firebase_storage.Reference storageRef =
-          firebase_storage.FirebaseStorage.instance.ref().child(
-                'profile_images/${DateTime.now().millisecondsSinceEpoch}.jpg',
-              );
-      final firebase_storage.UploadTask uploadTask =
-          storageRef.putFile(_selectedImage!);
-      await uploadTask.whenComplete(() async {
-        final String imageUrl = await storageRef.getDownloadURL();
-        print('Uploaded image URL: $imageUrl');
-        // Save the image URL to Firestore or perform any other operation
-        await _saveImageUrlToFirestore(imageUrl);
-      });
+  Future<void> _uploadImageToFirebaseStorage() async {
+    try {
+      if (_selectedImage != null) {
+        final firebase_storage.Reference storageRef =
+            firebase_storage.FirebaseStorage.instance.ref().child(
+                  'profile_images/${DateTime.now().millisecondsSinceEpoch}.jpg',
+                );
+        final firebase_storage.UploadTask uploadTask =
+            storageRef.putFile(_selectedImage!);
+        await uploadTask.whenComplete(() async {
+          final String imageUrl = await storageRef.getDownloadURL();
+          print('Uploaded image URL: $imageUrl');
+          await _saveImageUrlToFirestore(imageUrl);
+        });
+      }
+    } catch (e) {
+      print('Error uploading image to Firebase Storage: $e');
     }
-  } catch (e) {
-    print('Error uploading image to Firebase Storage: $e');
-    // Handle error
   }
-}
 
-Future<void> _saveImageUrlToFirestore(String imageUrl) async {
-  try {
-    String uid = FirebaseAuth.instance.currentUser!.uid;
-    await FirebaseFirestore.instance.collection('users').doc(uid).update({
-      'profileImageUrl': imageUrl,
-    });
-  } catch (e) {
-    print('Error saving image URL to Firestore: $e');
-    // Handle error
+  Future<void> _saveImageUrlToFirestore(String imageUrl) async {
+    try {
+      String uid = FirebaseAuth.instance.currentUser!.uid;
+      await FirebaseFirestore.instance.collection('users').doc(uid).update({
+        'profileImageUrl': imageUrl,
+      });
+    } catch (e) {
+      print('Error saving image URL to Firestore: $e');
+    }
   }
-}
 
   void _saveProfileData() async {
     try {
@@ -114,93 +125,244 @@ Future<void> _saveImageUrlToFirestore(String imageUrl) async {
       String name = _nameController.text;
       String email = _emailController.text;
       String dob = _dobController.text;
-      // Implement logic to save profile data to Firestore
+
       await FirebaseFirestore.instance.collection('users').doc(uid).set({
         'Name': name,
         'Email': email,
         'Date Of Birth': dob,
-        // Add more fields as needed
+        'Exercise Frequency': exerciseFrequency,
+        'Weight': weight,
+        'Height': height,
       });
-      // Optionally, show a success message to the user
+
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Profile saved successfully')),
       );
     } catch (e) {
-      // Handle errors
       print('Error saving profile data: $e');
-      // Optionally, show an error message to the user
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Failed to save profile data')),
       );
     }
   }
 
+  double calculateWaterIntake() {
+    double waterIntake = weight * 0.033;
+    switch (exerciseFrequency) {
+      case '1-2 days a week':
+        waterIntake += 0.5;
+        break;
+      case '3-4 days a week':
+        waterIntake += 1.0;
+        break;
+      case '5-6 days a week':
+        waterIntake += 1.5;
+        break;
+      case 'Every day':
+        waterIntake += 2.0;
+        break;
+    }
+    return waterIntake;
+  }
+
+  void _showWeightPicker(BuildContext context) {
+    int tempWeight = weight.toInt();
+    showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 300.0,
+          color: Colors.white, // Set background color
+          child: Column(
+            children: [
+              Container(
+                height: 32.0,
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      weight = tempWeight.toDouble();
+                    });
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'Done',
+                    style: TextStyle(
+                      color: Colors.blue, // Button text color
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: CupertinoPicker(
+                  itemExtent: 32.0,
+                  onSelectedItemChanged: (int index) {
+                    tempWeight = index + 30; // Adjust to your minimum value
+                  },
+                  children: List<Widget>.generate(121, (int index) {
+                    return Center(child: Text('${index + 30} kg',style: TextStyle(color: tempWeight == index + 30 ? Colors.blue : Colors.black,)));
+                  }),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _showHeightPicker(BuildContext context) {
+    int tempHeight = height.toInt();
+    showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200.0,
+          color: Colors.white, // Set background color
+          child: Column(
+            children: [
+              Container(
+                height: 32.0,
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      height = tempHeight.toDouble();
+                    });
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'Done',
+                    style: TextStyle(
+                      color: Colors.blue, // Button text color
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: CupertinoPicker(
+                  itemExtent: 32.0,
+                  diameterRatio: 1.0,
+                  onSelectedItemChanged: (int index) {
+                    tempHeight = index + 100; // Adjust to your minimum value
+                  },
+                  children: List<Widget>.generate(121, (int index) {
+                    return Center(child: Text('${index + 100} cm', style: TextStyle(color: tempHeight == index + 100 ? Colors.blue : Colors.black,)));
+                  }),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          GestureDetector(
-            onTap: _selectImage,
-            child: Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.grey, // Placeholder color
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: _selectImage,
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey, // Placeholder color
+                ),
+                child: _selectedImage != null
+                    ? Image.file(
+                        _selectedImage!,
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      )
+                    : Icon(Icons.person, size: 50), // Placeholder icon
               ),
-              child: _selectedImage != null
-                  ? Image.network(
-                      _selectedImage!.path,
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    )
-                  : Icon(Icons.person, size: 50), // Placeholder icon
             ),
-          ),
-          SizedBox(height: 16.0),
-          TextField(
-            controller: _nameController,
-            decoration: const InputDecoration(
-              labelText: 'Name',
-              border: OutlineInputBorder(), // Add border to text field
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                border: OutlineInputBorder(), // Add border to text field
+              ),
             ),
-          ),
-          SizedBox(height: 16.0), // Add spacing between fields
-          TextField(
-            controller: _emailController,
-            decoration: const InputDecoration(
-              labelText: 'Email',
-              border: OutlineInputBorder(), // Add border to text field
+            SizedBox(height: 16.0), // Add spacing between fields
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(), // Add border to text field
+              ),
             ),
-          ),
-          SizedBox(height: 16.0),
-          InkWell(
-            onTap: _selectDate,
-            child: IgnorePointer(
-              child: TextFormField(
-                controller: _dobController,
-                decoration: const InputDecoration(
-                  labelText: 'Date of Birth',
-                  border: OutlineInputBorder(), // Add border to text field
+            SizedBox(height: 16.0),
+            InkWell(
+              onTap: _selectDate,
+              child: IgnorePointer(
+                child: TextFormField(
+                  controller: _dobController,
+                  decoration: const InputDecoration(
+                    labelText: 'Date of Birth',
+                    border: OutlineInputBorder(), // Add border to text field
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 20.0),
-          Center(
-            child: ElevatedButton(
-              onPressed: _saveProfileData,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.lightBlue, // Set button color to light blue
-              ),
-              child: const Text('Save Profile'),
+            SizedBox(height: 16.0),
+            DropdownButton<String>(
+              value: exerciseFrequency,
+              onChanged: (String? newValue) {
+                setState(() {
+                  exerciseFrequency = newValue!;
+                });
+              },
+              items:
+                  exerciseOptions.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
-          ),
-        ],
+            SizedBox(height: 16.0),
+            ListTile(
+              title: Text('Weight: ${weight.toStringAsFixed(1)} kg'),
+              trailing: Icon(Icons.edit),
+              onTap: () => _showWeightPicker(context),
+            ),
+            ListTile(
+              title: Text('Height: ${height.toStringAsFixed(1)} cm'),
+              trailing: Icon(Icons.edit),
+              onTap: () => _showHeightPicker(context),
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: ElevatedButton(
+                onPressed: _saveProfileData,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      Colors.lightBlue, // Set button color to light blue
+                ),
+                child: const Text('Save Profile'),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Text(
+                'Recommended Water Intake: ${calculateWaterIntake().toStringAsFixed(2)} liters/day',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
